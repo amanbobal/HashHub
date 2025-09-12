1e5d4f7344bf455fbd1621a19aad9d8ee53eebd0
KRISHNA <kraggr2909@gmail.com>
2025-09-12 04:59:19 +0000

    _redirects file is added

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Landing, Login, Signup } from './pages/index.js'
+import { Home, Landing, Login, Signup } from './pages/index.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,6 +13,7 @@ createRoot(document.getElementById('root')).render(
           <Route index element={<Landing />} /> 
           <Route path='login' element={<Login />} />
           <Route path='signup' element={<Signup />} />
+          <Route path='home' element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
