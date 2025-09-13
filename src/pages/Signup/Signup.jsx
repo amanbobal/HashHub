06bc652c3a8813d13bbc77244c2f90e152b6b361
KRISHNA <kraggr2909@gmail.com>
2025-09-13 09:02:44 +0530

    signup form done

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastname: '',
+    email: '',
+    password: '',
+    image: null,
+  })
+  const [errors, setErrors] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({...formData, [name] : value});
+    setErrors({...errors, [name] : ''});
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = validateForm();
+    if(Object.keys(newErrors).length > 0){
+      setErrors(newErrors);
+      return;
+    }
+  }
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!formData.email) newErrors.email = 'Email is required';
+    else if (!/\S+@\S+\.\S+/.test(formData.email)) newErrors.email = 'Invalid email format';
+    if (!formData.password) newErrors.password = 'Password is required';
+    else if (formData.password.length < 6) newErrors.password = 'Password must be at least 6 characters';
+    return newErrors;
+  }
   return (
     <>
       {/* <Navbar /> */}
@@ -12,11 +45,18 @@ function Signup() {
               Sign Up
             </h1>
 
-            <form className="mx-5">
+            <form className="mx-5" onSubmit={handleSubmit}>
               {/* Error Message */}
-              <div className="text-center text-red-500 font-medium mb-4 rounded-md bg-red-100/10 px-3 py-3 border border-red-500/30">
-                This is an error message!
-              </div>
+              {Object.keys(errors).length > 0 && (
+
+                <div className="text-center text-red-500 font-medium mb-4 rounded-md bg-red-100/10 px-3 py-3 border border-red-500/30">
+                  {errors.email || errors.password || 'Please check your input'}
+                </div>
+              )
+
+
+
+              }
 
               {/* Name details */}
               <div className="flex w-full flex-col sm:flex-row">
@@ -29,6 +69,7 @@ function Signup() {
                     name="firstname"
                     id="firstName"
                     placeholder="First Name"
+                    onChange={handleChange}
                     className="p-2 bg-transparent text-white border border-gray-600 rounded-md focus:border-white outline-none"
                   />
                 </div>
@@ -66,24 +107,31 @@ function Signup() {
                   Password
                 </label>
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   name="password"
                   id="password"
                   placeholder="Enter new password"
                   className="p-2 bg-transparent text-white border border-gray-600 rounded-md focus:border-white outline-none"
                 />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="16"
-                  width="16"
-                  className="absolute right-4 top-[65%] translate-y-[-10%] text-gray-400"
-                  viewBox="0 0 640 640"
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-4 top-[65%] translate-y-[-10%] text-gray-400 hover:text-teal-400 transition-colors"
                 >
-                  <path
-                    fill="#aaa"
-                    d="M320 96C239.2 96 174.5 132.8 127.4 176.6C80.6 220.1 49.3 272 34.4 307.7C31.1 315.6 31.1 324.4 34.4 332.3C49.3 368 80.6 420 127.4 463.4C174.5 507.1 239.2 544 320 544C400.8 544 465.5 507.2 512.6 463.4C559.4 419.9 590.7 368 605.6 332.3C608.9 324.4 608.9 315.6 605.6 307.7C590.7 272 559.4 220 512.6 176.6C465.5 132.9 400.8 96 320 96zM176 320C176 240.5 240.5 176 320 176C399.5 176 464 240.5 464 320C464 399.5 399.5 464 320 464C240.5 464 176 399.5 176 320zM320 256C320 291.3 291.3 320 256 320C244.5 320 233.7 317 224.3 311.6C223.3 322.5 224.2 333.7 227.2 344.8C240.9 396 293.6 426.4 344.8 412.7C396 399 426.4 346.3 412.7 295.1C400.5 249.4 357.2 220.3 311.6 224.3C316.9 233.6 320 244.4 320 256z"
-                  />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="16"
+                    width="16"
+                    viewBox="0 0 640 640"
+                    fill={showPassword ? '#2dd4bf' : '#aaa'}
+                  >
+                    {showPassword ? (
+                      <path d="M320 96C239.2 96 174.5 132.8 127.4 176.6C80.6 220.1 49.3 272 34.4 307.7C31.1 315.6 31.1 324.4 34.4 332.3C49.3 368 80.6 420 127.4 463.4C174.5 507.1 239.2 544 320 544C400.8 544 465.5 507.2 512.6 463.4C559.4 419.9 590.7 368 605.6 332.3C608.9 324.4 608.9 315.6 605.6 307.7C590.7 272 559.4 220 512.6 176.6C465.5 132.9 400.8 96 320 96zM176 320C176 240.5 240.5 176 320 176C399.5 176 464 240.5 464 320C464 399.5 399.5 464 320 464C240.5 464 176 399.5 176 320z" />
+                    ) : (
+                      <path d="M320 96C239.2 96 174.5 132.8 127.4 176.6C80.6 220.1 49.3 272 34.4 307.7C31.1 315.6 31.1 324.4 34.4 332.3C49.3 368 80.6 420 127.4 463.4C174.5 507.1 239.2 544 320 544C400.8 544 465.5 507.2 512.6 463.4C559.4 419.9 590.7 368 605.6 332.3C608.9 324.4 608.9 315.6 605.6 307.7C590.7 272 559.4 220 512.6 176.6C465.5 132.9 400.8 96 320 96zM176 320C176 240.5 240.5 176 320 176C399.5 176 464 240.5 464 320C464 399.5 399.5 464 320 464C240.5 464 176 399.5 176 320zM320 256C320 291.3 291.3 320 256 320C244.5 320 233.7 317 224.3 311.6C223.3 322.5 224.2 333.7 227.2 344.8C240.9 396 293.6 426.4 344.8 412.7C396 399 426.4 346.3 412.7 295.1C400.5 249.4 357.2 220.3 311.6 224.3C316.9 233.6 320 244.4 320 256z" />
+                    )}
+                  </svg>
+                </button>
               </div>
 
               {/* Image input */}
